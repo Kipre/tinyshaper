11c83ef532e5259b3a450d690d127833041e1c6c
Kipre <kipr@live.ru>
2022-05-15 19:19:33 +0200

    wip

--- js/3d.js
+++ js/3d.js
@@ -1,14 +1,23 @@
-import*as surf from './surf.js';
-import {TrackballControls} from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/examples/jsm/controls/TrackballControls.js';
+import * as surf from './surf.js';
+import {config} from './ui.js';
+import * as THREE from "three";
+import { TrackballControls } from 'https://unpkg.com/three@0.140.2/examples/jsm/controls/TrackballControls.js';
+
 
 const scene = new THREE.Scene();
 const canvas = document.getElementById('threed');
-const camera = new THREE.PerspectiveCamera(75,canvas.offsetWidth / canvas.offsetHeight,0.1,100);
-const controls = new TrackballControls(camera,canvas);
 
-const renderer = new THREE.WebGLRenderer({
-    canvas
-});
+const {x: pad, y: vert} = halves(canvas.offsetWidth, canvas.offsetHeight);
+
+const camera = new THREE.OrthographicCamera( -pad, pad, vert, -vert, -1, 1000 );
+camera.position.set(1, 1, 1);
+
+const controls = new TrackballControls(camera, canvas);
+controls.rotateSpeed = 2.0;
+controls.zoomSpeed = 1.2;
+controls.noPan = true;
+
+const renderer = new THREE.WebGLRenderer({canvas, alpha: true});
 
 const material = new THREE.MeshPhongMaterial();
 
@@ -21,7 +30,6 @@ light2.position.set(-10, -10, -5);
 scene.add(light2);
 
 const ambient = new THREE.AmbientLight(0x404040);
-// soft white ambient
 scene.add(ambient);
 
 let mesh;
@@ -40,26 +48,64 @@ export function display3D(board) {
     mesh = new THREE.Mesh(geometry,material);
     scene.add(mesh);
 
-    init();
+    onResize();
     animate();
 }
 
-function init() {
+function halves(width, height) {
+    const x = 0.5 * (1 + config.padding / (width / 2 - config.padding));
+    const y = height * x / width;
+    return {x, y};
+}
+
+function onResize() {
     const parent = document.querySelector('.content');
-    const width = parent.offsetWidth;
-    const height = parent.offsetHeight;
+    const width = parent.offsetWidth,
+        height = parent.offsetHeight;
     renderer.setSize(width, height);
-    camera.aspect = width / height;
-    camera.up = new THREE.Vector3(1,0,0);
-    camera.position.set(0, 0, 560 / width);
+
+    const {x: pad, y: vert} = halves(width, height);
+
+    camera.left = - pad;
+    camera.right = pad;
+    camera.top = vert;
+    camera.bottom = - vert;
+    camera.zoom = 1;
+
     camera.updateProjectionMatrix();
     controls.update();
 }
-
-window.addEventListener('resize', init);
 
 function animate() {
     requestAnimationFrame(animate);
     controls.update();
     renderer.render(scene, camera);
 }
+
+window.addEventListener('resize', onResize);
+
+document.getElementById('set-z').addEventListener('click', () => {
+    camera.position.set(0, 0, 1);
+    camera.up.x = 0;
+    camera.up.y = 0;
+    camera.up.z = 1;
+    camera.lookAt(0, 0, 0)
+    // controls.zoom = 1;
+    controls.update();
+})
+
+document.getElementById('set-y').addEventListener('click', () => {
+    camera.position.set(-1, 0, 0);
+    // camera.up.set(0, 0, 1);
+    // camera.lookAt(0, 0, 0)
+    // controls.zoom = 1;
+    controls.update();
+})
+
+document.getElementById('set-x').addEventListener('click', () => {
+    camera.position.set(0, -1, 0);
+    // camera.up.set(0,1,0);
+    // camera.lookAt(0, 0, 0)
+    // controls.zoom = 2;
+    controls.update();
+})
--- js/ui.js
+++ js/ui.js
@@ -1,7 +1,7 @@
 import * as d3 from "https://cdn.skypack.dev/d3@7";
 import {roots, siblingPosition, evaluate} from './surf.js';
 
-const config = {
+export const config = {
     pointStrokeWidth: 1,
     selectRadius: 7,
     pointRadius: 4,
@@ -34,8 +34,6 @@ export function setup(board, onDragEnd) {
             width: board.z[3].y * board.width
         }
     }
-
-    document.getElementById('dims-control').setDims(board);
 
     const {width, height} = profiles[state.profile];
     const points = board[state.profile];
@@ -153,225 +151,13 @@ export function setup(board, onDragEnd) {
             .on("start", ({subject})=>subject && svg.style("cursor", "grabbing"))
             .on("drag", ({subject, dx, dy})=>{
                 subject(dx, dy);
-                onDragEnd?.();
             })
             .on("end", ()=>{
                 svg.style("cursor", "grab");
+                onDragEnd?.();
             })
             .on("start.render drag.render end.render", update));
     }
 
     window.onresize = updateViewport;
 }
-
-const diyElementsStyle = `
-
-
-input, .box {
-    text-align:center;
-    border-radius: 5px;
-    width: 70px;
-    margin: 2px;
-    font-size: 15px;
-    font-weight: 500;
-    display: inline-block;
-    float: left;
-}
-
-.box {
-    cursor: pointer;
-    width: 74px;
-    position: relative;
-    background: #ffffff;
-    color: #000000;
-    padding: 3px 2px;
-}
-
-.switch {   
-    position: absolute;
-    z-index: 10;
-    left: 0px;
-    top: 0px;
-    width: 35px;
-    height: 20px;
-    background: rgb(248 248 248 / 0%);
-    border-width: 2px;
-    border-style: inset;
-    border-color: rgb(118, 118, 118);
-    border-radius: 5px;
-    transition: .2s;
-}
-
-.checked > .switch {
-    transform: translateX(39px);
-}
-
-.disabled > .switch {
-    border-color: rgba(118, 118, 118, 0);
-    transition: 0s;
-}
-
-.disabled {   
-    background: rgba(239, 239, 239, 0.3);
-    color: rgb(110 110 110);
-    transition: .2s;
-    border-style: inset;
-    border-color: rgba(118, 118, 118, 0.3);
-    border-width: 2px;
-    padding: 1px 0px;
-    transition: .0s;
-}
-
-
-.container {
-    height: 30px;
-}
-
-input[type='submit'] {
-    position: absolute; 
-    left: -200px;
-}
-
-label{
-    display: inline-block;
-    float: left;
-    clear: left;
-    width: 100px;
-    font-size: 14px;
-    text-align: right;
-    padding: 4px;
-}
-
-@media (prefers-color-scheme: dark) {
-    input, .box {
-        background: #5c585d;
-        color: #e8dbc6;
-    }
-
-    .switch {
-        border-style: solid;
-        border-color: rgb(181 181 181);
-    }
-
-    input:disabled, .disabled {
-        cursor: default;
-        background: #3f3c40;
-        color: rgb(110 110 110);
-    }
-}
-`;
-
-customElements.define('dim-input', class extends HTMLElement {
-    constructor() {
-        super();
-
-        const shadowRoot = this.attachShadow({
-            mode: 'open'
-        });
-
-        this.name = this.getAttribute('name');
-        const form = document.createElement('form');
-        const div = document.createElement('div');
-        div.classList.add('container');
-        const input = document.createElement('input');
-        const label = document.createElement('label');
-        const css = document.createElement('style');
-        css.textContent = diyElementsStyle;
-
-        shadowRoot.appendChild(css);
-        shadowRoot.appendChild(form);
-        form.appendChild(div);
-        div.appendChild(label);
-        div.appendChild(input);
-
-        input.value = this.textContent;
-        input.disabled = true;
-        label.innerText = this.name.charAt(0).toUpperCase() + this.name.slice(1);
-
-        const showValue = ()=>{
-            if (this.point) {
-                input.value = round(this.point[this.name]);
-                input.disabled = !this.point['freedom' + this.name.charAt(0).toUpperCase()];
-            } else {
-                input.value = ''
-                input.disabled = true;
-            }
-        }
-
-        document.addEventListener('pointselected', (e)=>{
-            this.point = e.detail?.point;
-            this.movePoint = e.detail?.move;
-            showValue();
-        }
-        );
-
-        document.addEventListener('pointmove', showValue);
-
-        form.addEventListener('submit', e=>{
-            e.preventDefault();
-            if (this.point) {
-                this.movePoint(parseFloat(input.value) - this.point[this.name], this.name);
-            }
-        }
-        );
-    }
-}
-);
-
-customElements.define('dims-input', class extends HTMLElement {
-
-    board;
-
-    constructor() {
-        super();
-
-        const shadowRoot = this.attachShadow({
-            mode: 'open'
-        });
-
-        const form = document.createElement('form');
-        const style = document.createElement('style');
-        style.textContent = diyElementsStyle;
-        shadowRoot.appendChild(style);
-        shadowRoot.appendChild(form);
-        this.inputs = {};
-        this.dims = ['length', 'width', 'thickness'];
-
-        for (const dim of this.dims) {
-            const div = document.createElement('div');
-            const input = document.createElement('input');
-            const label = document.createElement('label');
-
-            this.inputs[dim] = input;
-            input.value = '';
-            label.innerText = dim.charAt(0).toUpperCase() + dim.slice(1);
-
-            form.appendChild(div);
-            div.appendChild(label);
-            div.appendChild(input);
-
-        }
-
-        const submit = document.createElement('input');
-        submit.setAttribute('type', 'submit')
-        form.appendChild(submit)
-
-        form.addEventListener('submit', e=>{
-            e.preventDefault();
-            for (const dim of this.dims) {
-                this.board[dim] = parseFloat(this.inputs[dim].value);
-            }
-            const event = new Event('dimschange');
-            document.dispatchEvent(event);
-        }
-        );
-    }
-
-    setDims(board) {
-        this.board = board;
-        for (const dim of this.dims) {
-            this.inputs[dim].value = board[dim];
-        }
-    }
-}
-);
